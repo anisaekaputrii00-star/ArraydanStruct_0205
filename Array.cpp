30facbbd001889684b2d09bc5d30da6b17caae5f
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-06 18:06:08 +0700

    menampilkan data satu per satu

--- Array.cpp
+++ Array.cpp
@@ -29,4 +29,11 @@ int main() {
     count << "data index 4 = ";
     cin  >> datamu[4];
     count << endl;
-    //menampilkan dat satu per satu
+   
+    //menampilkan data satu per satu
+    
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
